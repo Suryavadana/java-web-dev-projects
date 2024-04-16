e5d59b6d2062fb7905770917d9751db5ce3c8e2c
Surya Vadana Tetali Venkata <suryavadana5@outlook.com>
2024-04-15 20:00:10 -0500

    Studio

--- unit-testing/studio/balanced-brackets/src/test/java/org/launchcode/BalancedBracketsTest.java
+++ unit-testing/studio/balanced-brackets/src/test/java/org/launchcode/BalancedBracketsTest.java
@@ -7,7 +7,65 @@ import static org.junit.jupiter.api.Assertions.*;
 class BalancedBracketsTest {
     //TODO: add tests here
     @Test
-    public void emptyTest() {
-        assertEquals(true, true);
+    public void empty() {
+        assertTrue(BalancedBrackets.hasBalancedBrackets(""));
     }
+    @Test
+    public void emptyTest() {
+        assertTrue( BalancedBrackets.hasBalancedBrackets("[]"));
+    }
+
+    @Test
+    public void balancedBrackets() {
+        assertTrue(BalancedBrackets.hasBalancedBrackets("[LaunchCode]"));
+    }
+
+    @Test
+    public void balancedBracketsWithText() {
+        assertTrue(BalancedBrackets.hasBalancedBrackets("Launch[Code]"));
+    }
+
+    @Test
+    public void balancedBracketsAtBeginning() {
+        assertTrue(BalancedBrackets.hasBalancedBrackets("[]LaunchCode"));
+    }
+
+    @Test
+    public void balancedBracketsAtENd() {
+        assertTrue(BalancedBrackets.hasBalancedBrackets("LaunchCode[]"));
+    }
+
+
+
+    @Test
+   public void onlyOpeningBracket() {
+        assertFalse(BalancedBrackets.hasBalancedBrackets("[LaunchCode"));
+    }
+
+    @Test
+    public void testOnlyClosingBrackets() {
+        assertFalse(BalancedBrackets.hasBalancedBrackets("LaunchCode]"));
+    }
+
+
+    @Test
+    public void onlyOpenBracket() {
+        assertFalse(BalancedBrackets.hasBalancedBrackets("["));
+    }
+
+    @Test
+   public void closedOpenBrackets() {
+        assertTrue(BalancedBrackets.hasBalancedBrackets("]["));
+    }
+
+    @Test
+    public void onlyClosedBracket() {
+        assertFalse(BalancedBrackets.hasBalancedBrackets("]"));
+    }
+
+    @Test
+    public void onlyClosedBrackets() {
+        assertFalse(BalancedBrackets.hasBalancedBrackets("]]"));
+    }
+
 }
