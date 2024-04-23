d60c5599ff5058e0fbdca82a43da60983ffd928c
Surya Vadana Tetali Venkata <suryavadana5@outlook.com>
2024-04-23 13:41:08 -0500

    Studio

--- interfaces/studio/spinning-discs-studio/src/main/java/org/launchcode/CD.java
+++ interfaces/studio/spinning-discs-studio/src/main/java/org/launchcode/CD.java
@@ -1,8 +1,26 @@
 package org.launchcode;
 
-public class CD {
+public class CD extends BaseDisc implements OpticalDisc {
     // TODO: Implement your custom interface.
 
     // TODO: Determine which fields, methods, and constructors can be extended from the base class and which ones
     //  need to be declared separately.
+
+    String diskType;
+    String contents;
+
+    public CD(String aName, int maxCapacity, String aType, int someUsedCapacity) {
+        super(aName, maxCapacity, aType, someUsedCapacity);
+    }
+    @Override
+    public void spinDisc() {
+        System.out.println("A CD spins at rate 200-500 rpm.");
+    }
+
+    @Override
+    public void readData() {
+        System.out.println("Play a game?");
+    }
+
+
 }
--- interfaces/studio/spinning-discs-studio/src/main/java/org/launchcode/Main.java
+++ interfaces/studio/spinning-discs-studio/src/main/java/org/launchcode/Main.java
@@ -6,5 +6,20 @@ public class Main {
         // TODO: Declare and initialize a CD and a DVD object.
 
         // TODO: Call each CD and DVD method to verify that they work as expected.
+
+        CD cd = new CD("CD example", 700, "CD-R", 350);
+        DVD dvd = new DVD("DVD example", 4700, "DVD-R", 1450);
+
+        // TODO: Call each CD and DVD method to verify that they work as expected.
+        cd.spinDisc();
+        dvd.spinDisc();
+
+        cd.readData();
+        dvd.readData();
+
+        System.out.println(cd.writeData(275));
+        System.out.println(dvd.writeData(8000));
+
+
     }
 }
