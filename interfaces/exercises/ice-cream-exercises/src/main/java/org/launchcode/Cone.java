34090a916789bffa65f82f69c31d602b586ea428
Surya Vadana Tetali Venkata <suryavadana5@outlook.com>
2024-04-29 20:03:53 -0500

    Debugging

--- interfaces/exercises/ice-cream-exercises/src/main/java/org/launchcode/Cone.java
+++ interfaces/exercises/ice-cream-exercises/src/main/java/org/launchcode/Cone.java
@@ -6,4 +6,39 @@ public class Cone extends Ingredient {
     public Cone(String aName, double aCost, ArrayList<String> someAllergens){
         super (aName, aCost, someAllergens);
     }
+
+    @Override
+    public String getName() {
+        return super.getName();
+    }
+
+    @Override
+    public void setName(String name) {
+        super.setName(name);
+    }
+
+    @Override
+    public double getCost() {
+        return super.getCost();
+    }
+
+    @Override
+    public void setCost(double cost) {
+        super.setCost(cost);
+    }
+
+    @Override
+    public ArrayList<String> getAllergens() {
+        return super.getAllergens();
+    }
+
+    @Override
+    public void setAllergens(ArrayList<String> allergens) {
+        super.setAllergens(allergens);
+    }
+
+    @Override
+    public String toString() {
+        return super.toString();
+    }
 }
